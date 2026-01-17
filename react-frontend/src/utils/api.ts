047c0c75876101d345aac10017a1dbb3f2ccd26f
senyich <senyacm@gmail.com>
2026-01-17 18:17:27 +0700

    update

--- react-frontend/src/utils/api.ts
+++ react-frontend/src/utils/api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 import type { Token } from "../config/types";
 
 // const BASE_URI = import.meta.env.VITE_API_BASE_URI || "http://localhost:5000";
-const BASE_URI = ""
+const BASE_URI = "/api";
 
 export async function getSymbolsAsync() {
   try {
